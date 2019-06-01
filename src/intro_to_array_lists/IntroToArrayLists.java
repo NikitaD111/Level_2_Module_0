27495330ecb3bd55b714aea5ae1902f0264f8250
League Student <league@iMac.attlocal.net>
2019-05-31 17:53:52 -0700

    IntroToArrayLists

--- src/intro_to_array_lists/IntroToArrayLists.java
+++ src/intro_to_array_lists/IntroToArrayLists.java
@@ -11,9 +11,9 @@ public class IntroToArrayLists {
 		//2. Add five Strings to your list
 		array.add("ooga");
 		array.add("owawa");
-		array.add("angi");
-		array.add("ogma");
-		array.add("argau");
+		array.add("anei");
+		array.add("oema");
+		array.add("areau");
 		//3. Print all the Strings using a standard for-loop
 		for(int i = 0; i < array.size(); i++){
 			System.out.println(array.get(i));
@@ -25,11 +25,22 @@ public class IntroToArrayLists {
 		}
 		//5. Print only the even numbered elements in the list.
 		for(int i = 0; i < array.size(); i++) {
-			
+			if (i%2 == 0) {
+				System.out.println(array.get(i));
+			}
 		}
 		//6. Print all the Strings in reverse order.
+		for(int i = 5; i < array.size(); i--){
+			System.out.println(array.get(i));
+		}
 		
 		//7. Print only the Strings that have the letter 'e' in them.
+		for (int i = 0; i < array.size(); i++) {
+			if(array.get(i).contains("e")) {
+				System.out.println(array.get(i));
+				
+			}
+		}
 	}
 	}
 
